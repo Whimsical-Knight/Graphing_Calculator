08fcb5f590b887b29e645e35e3fc612ab4bdd9a6
Respectable Dragon <aebalken@icloud.com>
2026-04-01 14:57:28 -0600

    Switch to compose, added graphing functionality and scientific calc app functionality

--- app/src/main/java/com/example/cs3318_finalproject/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/example/cs3318_finalproject/viewmodel/CalculatorViewModel.kt
@@ -11,24 +11,20 @@ class CalculatorViewModel : ViewModel() {
     private val engine = CalculatorEngine()
 
     // reactive state container (trigger to update the UI when the expression value changes)
-    // this is for graphing calculator
     private val _expression = MutableStateFlow("")
     val expression: StateFlow<String> = _expression
 
     // reactive state container that is triggered by "=" sign
     // basically updates the UI
-    // this is for my scientific calculator
     private val _result = MutableStateFlow("")
     val result: StateFlow<String> = _result
 
     // when button is pressed, it is appended to the existing expression
-    // used for scientific calculator
     fun onInput(value: String) {
         _expression.value += value
     }
 
     // condition to clear expression when user enters "C"
-    // used for scientific calculator
     fun onClear() {
         _expression.value = ""
         _result.value = ""
@@ -36,7 +32,6 @@ class CalculatorViewModel : ViewModel() {
 
     // call the calculator engine when the user presses "=" sign
     // i.e., try to run the calculation
-    // this is for the scientific calculator
     fun onEquals() {
         _result.value = engine.evaluate(_expression.value)
     }
